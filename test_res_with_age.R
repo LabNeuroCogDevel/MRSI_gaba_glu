5f567f9fcd81ba24e55939964ee286a53aa13b23
WillForan <willforan@gmail.com>
2023-05-31 11:46:52 -0400

    📝 readme: add output files and gotchas

--- test_res_with_age.R
+++ test_res_with_age.R
@@ -4,16 +4,12 @@ source('mrsi_funcs.R')
 met_example <- read.csv('13MP20200207_LCMv2fixidx.csv') %>% mrsi_metqc() %>% mrsi_add_cols()
 
 test_that("res_with_age return types",{
-   res <- res_with_age(met_example, 'Glu.Cr', this_roi=1)
+   res <- res_with_age(met_example, 'Glu.Cr', return_df=TRUE)
    expect_equal(class(res),'data.frame')
-   expect_true(all(res$roi==1))
 
    res <- res_with_age(met_example, 'Glu.Cr')
    expect_equal(class(res),'array')
    expect_equal(length(res), nrow(met_example))
-
-   res <- res_with_age(met_example, 'Glu.Cr', return_df=TRUE)
-   expect_equal(class(res),'data.frame')
 })
 
 
