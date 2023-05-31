8cd627af10ade42fe42dbde0e208952deed109d7
WillForan <willforan@gmail.com>
2023-05-31 10:52:55 -0400

    🎨 readMRS.R: use mrsi_funcs.R

--- readMRS.R
+++ readMRS.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 pacman::p_load(readxl, dplyr, tidyr)
-select <- dplyr::select
+select <- dplyr::select  # make sure we're using the select we mean too
+source('mrsi_funcs.R') # mrsi_metqc, mrsi_add_cols
 
 # makes MRS variable from csv and xlsx
 csv_file <- "13MP20200207_LCMv2fixidx.csv"
@@ -21,49 +22,11 @@ MRS <- select(MRS, -bad)
 
 #keep only visit 1 people
 MRS <- MRS %>% filter(visitnum==1)
-#keep people's correct coordinates
-MRS <- MRS %>% filter(!is.na(roi))
 #get rid of people who are actually visit 2 but for some reason aren't filtered out
 MRS <- MRS %>% filter(ld8!="10195_20191205")
 
-# get rid of junk data noticed recently 
-MRS<- MRS %>% filter(Glu.Cr != 0)
+# remove rows where any of GPC.Cho.SD, NAA.NAAG.SD, Cr.SD, MM20.Cr have very bad values
+MRS <- MRS %>% mrsi_metqc()
 
-# save out a dataframe to share data after this step
-
-# Step 2 Outlier Detection - get rid of peole who have bad data for 3 major metabolite peaks - GPC+Cho, NAA+NAAG, Cr
-MRS<- filter(MRS, GPC.Cho.SD <= 10 | is.na(GPC.Cho.SD))
-MRS <- filter(MRS, NAA.NAAG.SD <= 10 | is.na(NAA.NAAG.SD))
-MRS <- filter(MRS, Cr.SD <= 10 | is.na(Cr.SD))
-
-# Step 3 Outlier Detection - get rid of people who have lots of macromolecule in their spectra, as that can create distortions
-MRS <- filter(MRS, MM20.Cr <= 3 | is.na(MM20.Cr))
-
-#make inverse age column
-MRS$invage <- 1/MRS$age
-#make age^2 column
-MRS$age2 <- (MRS$age - mean(MRS$age))^2
-
-
-## 3. and agegrp and region label
-##  NB. could remove R/L from names to later collapse/average across hemisphere
-##  13MP20200207_LCMv2fixidx.csv now includes 'label' column
-region_look <- list(
-  "1" = "RAntInsula",
-  "2" = "LAntInsula",
-  "3" = "RPostInsula",
-  "4" = "LPostInsula",
-  "5" = "RCaudate",
-  "6" = "LCaudate",
-  "7" = "ACC",
-  "8" = "MPFC",
-  "9" = "RDLPFC",
- "10" = "LDLPFC",
- "11" = "R STS",
- "12" = "L STS",
- "13" = "R Thalamus")
-
-MRS <- MRS %>%
-    mutate(agegrp = cut(age, breaks=c(0,16,22,Inf),
-                        labels=c("10-16","17-22", "23-30")),
-          region=unlist(region_look[as.character(roi)]))
+# add invage, age2 (squared), region (roi labels), dateNumeric, agegrp
+MRS <- MRS %>% mrsi_add_columns()
