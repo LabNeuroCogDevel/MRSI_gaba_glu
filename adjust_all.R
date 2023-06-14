29ed36595ce2b68b00085a5b67872c4582e5f8ec
WillForan <willforan@gmail.com>
2023-06-14 14:22:44 -0400

    ✨ adjust_all.R: add sd thresholds, dont model MM20, glu.gln, gpc.cho

--- adjust_all.R
+++ adjust_all.R
@@ -5,11 +5,12 @@ library(tidyr) # nest, unnest
 zscore_abs <- function(x) abs(scale(x,center=T,scale=T)[,1])
 
 # 20230531 - intially want GABA and Glu. but easy to add more
-mets_keep <- c("GABA","Glu","Gln","Cho","Glc", "MM20", "NAA")
+# 20230614 - remove MM20. regexp also removes Glu.Gln.Cr and GPC.Cho.Cr
+mets_keep <- c("GABA","Glu","Gln","Cho","Glc", "NAA")
 
 # select metabolites based on column name using dplyr::match w/ regular expression
-mets_patt <- paste0(collapse="|", mets_keep) # MM20|GABA|Glu|Gln|Cho|Glc
-mets_regex <- glue::glue('({mets_patt})\\.(Cr|SD)') # Cr ratio and SD
+mets_patt <- paste0(collapse="|", mets_keep) # GABA|Glu|Gln|Cho|Glc
+mets_regex <- glue::glue('^({mets_patt})\\.(Cr|SD)') # Cr ratio and SD
 
 # read in raw data. clean. and reduce columns to just those we care about
 mrs <- read.csv('13MP20200207_LCMv2fixidx.csv') %>%
@@ -19,17 +20,25 @@ mrs <- read.csv('13MP20200207_LCMv2fixidx.csv') %>%
 
 # longer with new cols 'met', 'Cr', and 'SD' (reshape to remove per metabolite columns)
 z_thres <- 3
+# added 20230614 (to match previous analysis)
+# 20 for Glu and GABA. guess for others
+# TODO: VALIDATE Glc and Cho SD thres
+sd_thres <- function(x)
+   case_when(
+             grepl('Glu|GABA',x) ~ 20,
+             grepl('Cho',x)      ~ 40, # TODO: check
+             grepl('Glc',x)      ~ 120,# TODO: check
+             .default=20)
 mrs_long <- longer_met_CrSD(mrs) %>%
    # collapse across hemispheres
    # conviently R,L are always hemisphere. others: MPFC ACC 
    mutate(biregion=gsub('^(R|L)','',region)) %>%
    # work on each region X met separetly
-   # TODO: should thresholding be just by met (not also by region?)
-    #      OR should group by biregion?
    group_by(met, region) %>% 
-   mutate(met_crz=zscore_abs(Cr),
-          met_sdz=zscore_abs(SD),
-          # TODO: also threshold on SD? or zscore of SD?
+   #NB. 'Cr' here is Cr ratio for a given metabolite
+   #    sd_thres is 20 for gaba and glu
+   mutate(Cr=ifelse(SD>sd_thres(met), NA, Cr),
+          met_crz=zscore_abs(Cr),
           Cr=ifelse(met_crz>=z_thres, NA, Cr))
 write.csv(mrs_long, "out/long_thres.csv", quote=F, row.names=F)
 
@@ -64,7 +73,8 @@ mrs_wide_adj_lat <- mrs_long_adj %>%
                 names_from = c("region", "met"),
                 values_from = c("gamadj","Cr","SD"),
                 names_glue = "{region}_{met}_{.value}") %>%
-    select(ld8,matches('gamadj'))
+    # 20230614: add Cr and SD, not just gamadj
+    select(ld8,matches('gamadj|Cr|SD'))
 
 # GMrat is not per metabolite. so we'll do the collapsing separetly
 mrs_wide_gmrat <- mrs_long_adj %>%
